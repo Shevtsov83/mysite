3bd7f12d98aed34de50eed4accf52b5f7bf788bf
Shevtsov83 <shnat2004@gmail.com>
2018-08-29 13:36:10 +0300

    Wednesday

--- polls/views.py
+++ polls/views.py
@@ -37,7 +37,7 @@ def vote(request, question_id):
         # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
             'question': question,
-            'error_message': "You didn't select a choice.",
+            'error_message': "Вы не сделали выбор варианта ответа",
         })
     else:
         selected_choice.votes += 1
